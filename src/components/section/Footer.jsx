d5a72847cb06a0155b19f8147010f81a29cf73dd
이한나 <814hanna@gmail.com>
2025-08-20 21:01:40 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer id="footer" role="contentinfo">
-      footer
+      <a href="mailto:814hanna@gmail.com" rel="noopener noreferrer">
+        814hanna@gmail.com
+      </a>
     </footer>
   );
 };
